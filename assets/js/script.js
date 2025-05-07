6ea568962ec80f88becdf0150444adde6202db1e
Garik_18 <garikarshakyam27@gmail.com>
2025-05-07 21:40:12 +0300

    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,7 +41,7 @@ let slideSwiper = new Swiper(".slideLab-slider", {
     direction: "vertical",
     slidesPerView: 1,
     mousewheel: true,
-    speed: 1600,
+    speed: 2600,
     allowTouchMove: true,
     on: {
         slideChange: function () {
